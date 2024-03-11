3e3df51006b1ffd70ec8f5b62590e04f606fff0c
«Vit-spb» <wwg79@mail.ru>
2024-03-12 00:39:00 +0300

    Creat method Main

--- Tasks/Program.cs
+++ Tasks/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System;
+class Program
+{
+static void Main()
+    {
+
+    string[] inputArray = { "Hello", "2", "world", ":-)","1234", "1567", "-2", "computer science", "Russia", "Denmark", "Kazan"};
+
+    string[] resultArray = FilterArrayByLength(inputArray);
+
+    Console.WriteLine("Исходный массив:");
+        PrintArray(inputArray);
+    Console.WriteLine();
+
+    Console.WriteLine("Новый массив (длина строк не более 3 символов):");
+        PrintArray(resultArray);
+
+    }   
+}
